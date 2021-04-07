ba3e69478683dccddd92a0e2fbafc0380280937d
Txuritan <txuritan@protonmail.com>
2021-04-07 01:15:37 -0400

    Remove thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,28 @@
-use thiserror::Error as ThisError;
-
-#[derive(ThisError, Debug)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("{0}")]
     Parsing(String),
-    #[error("{0}")]
     Cli(String),
-    #[error("{0}")]
-    IO(#[from] std::io::Error),
-    #[cfg(feature = "serde")]
-    #[error("{0}")]
-    Serde(#[from] serde_json::Error),
+    IO(std::io::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Parsing(err) => err.fmt(f),
+            Error::Cli(err) => err.fmt(f),
+            Error::IO(err) => err.fmt(f),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Parsing(_) => None,
+            Error::Cli(_) => None,
+            Error::IO(err) => Some(err),
+        }
+    }
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
